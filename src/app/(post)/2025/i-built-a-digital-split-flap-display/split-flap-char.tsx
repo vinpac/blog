01eac570a884b1b5a8ed03620f44ed72e1260844
Vinicius Pacheco <vin175pacheco@gmail.com>
2025-01-22 16:47:37 -0300

    fix: lint errors

--- src/app/(post)/2025/i-built-a-digital-split-flap-display/split-flap-char.tsx
+++ src/app/(post)/2025/i-built-a-digital-split-flap-display/split-flap-char.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import React, { useState, useEffect, useRef } from "react";
-import useSound from "use-sound";
+import React, { useState, useEffect } from "react";
 
 interface SplitFlapCharProps {
   targetChar: string;
